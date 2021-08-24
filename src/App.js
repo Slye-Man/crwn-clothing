124a9bbf482b3eaab6c1e6a6a2917a6c8343c7d2
Selaelo Sebola <selaelosebola67@gmail.com>
2021-08-24 06:15:36 +0200

    created project with create react app and added files for homepage

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import HomePage from './pages/homepage/homepage.component'
+import { Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
