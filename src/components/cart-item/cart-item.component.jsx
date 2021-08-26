b042bf50d555409cb470f326f6a68dfbf600612e
Selaelo Sebola <selaelosebola67@gmail.com>
2021-08-26 07:25:59 +0200

    fixed * for x

--- src/components/cart-item/cart-item.component.jsx
+++ src/components/cart-item/cart-item.component.jsx
@@ -7,7 +7,7 @@ const CartItem = ({ item: {imageUrl, price, name, quantity } }) => (
         <div className='item-details'>
             <span className='name'>{name}</span>
             <span className='price'>
-                {quantity} * R{price}
+                {quantity} x R{price}
             </span>
         </div>
     </div>
